b470ff0044af70f59b260c10ad60cdda4947d5f6
ismdelrod <36931359+ismdelrod@users.noreply.github.com>
2020-04-05 18:59:01 +0200

    commit fix ActionButton Warnings
     fix ActionButton Warnings

--- app/screens/VapeStores/VapeStores.js
+++ app/screens/VapeStores/VapeStores.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import ActionButton from "react-native-action-button";
 import * as firebase from "firebase";
+import ActionButton from "react-native-action-button";
+
+
+// Para Evitar los Warnings sobre el componente ActionButton
+import { YellowBox } from "react-native";
+import _ from "lodash";
+YellowBox.ignoreWarnings(["componentWillReceiveProps"]);
+const _console = _.clone(console);
+console.warn = (message) => {
+  if (message.indexOf("componentWillReceiveProps") <= -1) {
+    _console.warn(message);
+  }
+};
+//********************************************************** */
+
 
 export default VapeStores = (props) => {
   const { navigation } = props;
